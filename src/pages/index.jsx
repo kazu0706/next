a7804bcfcabd4963739d5c6f994d85a6bbf0b5d7
kazu0706 <kazuki.fuma0706@gmail.com>
2022-11-07 22:21:01 +0900

    ✨ useEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,19 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useCallback } from 'react';
+import { useEffect } from 'react';
 
 export default function Home() {
-    /**
-     * ボタンをクリックした際の処理
-     */
-    const handleClickButton = useCallback((e) => {
-        e.preventDefault();
+    useEffect(() => {
+        document.body.style.backgroundColor = "lightblue";
 
-        console.log("ボタン");
+        console.log("マウント");
+
+        return () => {
+            document.body.style.backgroundColor = "";
+
+            console.log("アンマウント");
+        }
     }, []);
 
     return (
@@ -23,8 +26,6 @@ export default function Home() {
 
             <Header />
 
-            <button onClick={handleClickButton}>ボタン</button>
-            
             <Main page={"index"} />
 
             <Footer />
