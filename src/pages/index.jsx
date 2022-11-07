5f1bd4164a7f808b2238547f8d1159b08a09c8ba
kazu0706 <kazuki.fuma0706@gmail.com>
2022-11-07 20:45:06 +0900

    ⚗️ 不要な記述でいつか消しますが試しにクリックイベントを記述

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,18 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
+import { useCallback } from 'react';
 
 export default function Home() {
+    /**
+     * ボタンをクリックした際の処理
+     */
+    const handleClickButton = useCallback((e) => {
+        e.preventDefault();
+
+        console.log("ボタン");
+    }, []);
+
     return (
         <div className={styles.container}>
             <Head>
@@ -12,6 +22,8 @@ export default function Home() {
             </Head>
 
             <Header />
+
+            <button onClick={handleClickButton}>ボタン</button>
             
             <Main page={"index"} />
 
