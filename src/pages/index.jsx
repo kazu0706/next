db225465f5cc60ac7a7a1415cebd93d4a07270d0
kazu0706 <kazuki.fuma0706@gmail.com>
2022-11-08 18:24:07 +0900

    ✨ useStateの利用

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,9 +3,19 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+    // カウント状態管理
+    const [count, setCount] = useState(0);
+
+    /**
+     * カウント増加処理
+     */
+    const handleClickCount = () => {
+        setCount(prevState => prevState + 1);
+    }
+
     useEffect(() => {
         document.body.style.backgroundColor = "lightblue";
 
@@ -25,6 +35,9 @@ export default function Home() {
             </Head>
 
             <Header />
+
+            <button onClick={handleClickCount}>カウント</button>
+            <h1>{count}</h1>
 
             <Main page={"index"} />
 
