e9e63ce1859a05e58afe6d11e5c1714ccd6dbc3c
kazu0706 <kazuki.fuma0706@gmail.com>
2022-11-08 19:47:24 +0900

    ✨ useCallbackの利用

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
     // カウント状態管理
@@ -12,21 +12,20 @@ export default function Home() {
     /**
      * カウント増加処理
      */
-    const handleClickCount = () => {
-        setCount(prevState => prevState + 1);
-    }
+    const handleClickCount = useCallback(() => {
+        if(count < 10)
+        {
+            setCount(prevState => prevState + 1);
+        }
+    }, [count]);
 
     useEffect(() => {
         document.body.style.backgroundColor = "lightblue";
 
-        console.log("マウント");
-
         return () => {
             document.body.style.backgroundColor = "";
-
-            console.log("アンマウント");
         }
-    }, []);
+    }, [count]);
 
     return (
         <div className={styles.container}>
