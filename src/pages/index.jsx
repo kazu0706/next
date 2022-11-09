3c904ba3b72ab85d5ef7b683b4f859d5a85f03fa
kazu0706 <kazuki.fuma0706@gmail.com>
2022-11-09 21:51:55 +0900

    ✨ カウントの表示・非表示機能を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,12 @@ export default function Home() {
     // カウント状態管理
     const [count, setCount] = useState(0);
 
+    // 入力文字状態管理
+    const [text, setText] = useState("");
+
+    // カウント表示状態管理
+    const [isShow, setIsShow] = useState(true);
+
     /**
      * カウント増加処理
      */
@@ -18,6 +24,27 @@ export default function Home() {
             setCount(prevState => prevState + 1);
         }
     }, [count]);
+
+    /**
+     * 入力要素に文字列を入力した際の処理
+     */
+    const handleInputText = useCallback((e) => {
+        if(e.target.value.length > 5)
+        {
+            alert("5文字以内で入力してください。");
+
+            return;
+        }
+
+        setText(e.target.value.trim());
+    }, []);
+
+    /**
+     * 表示・非表示をクリックした際の処理
+     */
+    const handleClickShow = () => {
+        setIsShow(prevState => !prevState);
+    }
 
     useEffect(() => {
         document.body.style.backgroundColor = "lightblue";
@@ -36,7 +63,12 @@ export default function Home() {
             <Header />
 
             <button onClick={handleClickCount}>カウント</button>
-            <h1>{count}</h1>
+            {isShow ? <h1>{count}</h1> : null} 
+            <input type="text" name="" id="" value={text} onChange={handleInputText} />
+
+            <div>
+                <button onClick={handleClickShow}>{isShow ? "非表示" : "表示"}</button>
+            </div>
 
             <Main page={"index"} />
 
